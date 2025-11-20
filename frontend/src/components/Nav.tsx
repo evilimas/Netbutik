6474a913ef909cd4bf4707d77664a90801bf3dbe
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-11-20 09:16:17 +0100

    added cart

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,6 +1,7 @@
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate } from 'react-router';
 
 function Nav() {
+  const navigate = useNavigate();
   return (
     <nav className="navigation">
       <img className="logo" src="/images/logo.png" alt="" />
@@ -10,6 +11,14 @@ function Nav() {
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </ul>
+      <div className="buttons">
+        <button className="login-button" onClick={() => navigate('/login')}>
+          Login
+        </button>
+        <button className="login-button" onClick={() => navigate('/cart')}>
+          Cart
+        </button>
+      </div>
     </nav>
   );
 }
