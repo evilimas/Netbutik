a8642f9dcbe9c105ab1e3eefc9e91ca5f16e50c1
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-11-25 14:41:18 +0100

    new routes

--- backend/src/routes/pets.routes.ts
+++ backend/src/routes/pets.routes.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import type { Router } from 'express';
-import { getPets, getPetById } from '../controllers/pets.controllers';
+import {
+  getPets,
+  getPetById,
+  getBreeds,
+  getSpecies,
+} from '../controllers/pets.controllers';
 import { validateNumericId } from '../middleware/pets.middleware';
 export const petRouter: Router = express.Router();
 
 petRouter.get('/', getPets);
+
+petRouter.get('/breeds', getBreeds);
+
+petRouter.get('/species', getSpecies);
 
 petRouter.get('/:id', validateNumericId, getPetById);
