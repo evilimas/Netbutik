31d82f475d323bfcf1c2e7d049455711e4980d11
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-11-28 13:10:50 +0100

    x

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import type { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { petRouter } from './routes/pets.routes';
+import { authRouter } from './routes/auth.routes';
 
 const PORT = process.env.PORT || 8000;
 const app: Express = express();
 
+app.use(express.json());
 app.use(cors());
 app.use('/pets', petRouter);
+app.use('/pets/auth', authRouter);
 
 app.use((req: Request, res: Response<{ message: string }>): void => {
   res.status(404).json({ message: 'Endpoint not found' });
