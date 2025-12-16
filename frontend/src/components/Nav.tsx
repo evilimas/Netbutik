082884278d050d447adac46001e8675674e141e3
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-12-16 14:15:09 +0100

    display username

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,7 +1,23 @@
 import { NavLink, useNavigate } from 'react-router';
+import { displayLogedUser } from '../services/authServices';
+import { useEffect, useState } from 'react';
 
 function Nav() {
+  const [username, setUsername] = useState<string | null>(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      const userData = await displayLogedUser();
+      if (userData && userData.username) {
+        setUsername(userData.username);
+      } else {
+        setUsername('Guest');
+      }
+    };
+    fetchUsername();
+  }, []);
+
   return (
     <nav className="navigation">
       <img className="logo" src="/images/logo.png" alt="" />
@@ -12,6 +28,7 @@ function Nav() {
         <NavLink to="/contact">Contact</NavLink>
       </ul>
       <div className="buttons">
+        <p className="welcome-message">Welcome, {username}</p>
         <button className="login-button" onClick={() => navigate('/login')}>
           Login
         </button>
