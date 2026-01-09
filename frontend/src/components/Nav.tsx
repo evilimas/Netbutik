45a52bd512ce850b65aa618416c8bf056537e55a
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-01-09 14:01:10 +0100

    refacturing

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,35 +3,43 @@ import { displayLogedUser } from '../services/authServices';
 import { useEffect, useState } from 'react';
 
 function Nav() {
-  const [username, setUsername] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>('Guest');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchUsername = async () => {
-      const userData = await displayLogedUser();
-      if (userData && userData.username) {
-        setUsername(userData.username);
-      } else {
-        setUsername('Guest');
-      }
-    };
-    fetchUsername();
-  }, []);
+  // useEffect(() => {
+  //   const fetchUsername = async () => {
+  //     const userData = await displayLogedUser();
+  //     if (userData && userData.username) {
+  //       setUsername(userData.username);
+  //     } else {
+  //       setUsername('Guest');
+  //     }
+  //   };
+  //   fetchUsername();
+  // }, []);
 
   return (
     <nav className="navigation">
       <img className="logo" src="/images/logo.png" alt="" />
+
       <ul>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/pets">Pets</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </ul>
+
       <div className="buttons">
         <p className="welcome-message">Welcome, {username}</p>
-        <button className="login-button" onClick={() => navigate('/login')}>
-          Login
-        </button>
+        {username === 'Guest' ? (
+          <button className="login-button" onClick={() => navigate('/login')}>
+            Login
+          </button>
+        ) : (
+          <button className="login-button" onClick={() => navigate('/logout')}>
+            Logout
+          </button>
+        )}
         <button className="login-button" onClick={() => navigate('/cart')}>
           Cart
         </button>
