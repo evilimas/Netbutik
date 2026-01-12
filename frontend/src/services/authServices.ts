5b009d07b34523c626a69352bb6855b8363906fd
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-01-12 14:31:49 +0100

    refacturering

--- frontend/src/services/authServices.ts
+++ frontend/src/services/authServices.ts
@@ -1,50 +1,50 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 
-const displayLogedUser = async () => {
-  try {
-    const response = await axios.get('http://localhost:8000/pets/auth/user', {
-      withCredentials: true,
-    });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching logged user:', error);
-    return null;
-  }
-};
+// const displayLogedUser = async () => {
+//   try {
+//     const response = await axios.get('http://localhost:8000/pets/auth/user', {
+//       withCredentials: true,
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching logged user:', error);
+//     return null;
+//   }
+// };
 
-const UserName = () => {
-  const [username, setUsername] = useState<string | null>(null);
-  useEffect(() => {
-    const fetchUsername = async () => {
-      const userData = await displayLogedUser();
-      if (userData && userData.username) {
-        setUsername(userData.username);
-      }
-    };
-    fetchUsername();
-  }, []);
-  return username;
-};
+// const UserName = () => {
+//   const [username, setUsername] = useState<string | null>(null);
+//   useEffect(() => {
+//     const fetchUsername = async () => {
+//       const userData = await displayLogedUser();
+//       if (userData && userData.username) {
+//         setUsername(userData.username);
+//       }
+//     };
+//     fetchUsername();
+//   }, []);
+//   return username;
+// };
 
-const checkAuth = async () => {
-  try {
-    const res = await fetch('http://localhost:8000/pets/auth/me');
+// const checkAuth = async () => {
+//   try {
+//     const res = await fetch('http://localhost:8000/pets/auth/me');
 
-    if (!res.ok) {
-      console.warn('Unexpected response:', res.status);
-      return false;
-    }
+//     if (!res.ok) {
+//       console.warn('Unexpected response:', res.status);
+//       return false;
+//     }
 
-    const user = await res.json();
-    if (!user.isLoggedIn) {
-      return false;
-    }
-    return user.name;
-  } catch (err) {
-    console.log(err, 'Auth check failed');
-    return false;
-  }
-};
+//     const user = await res.json();
+//     if (!user.isLoggedIn) {
+//       return false;
+//     }
+//     return user.name;
+//   } catch (err) {
+//     console.log(err, 'Auth check failed');
+//     return false;
+//   }
+// };
 
-export { displayLogedUser, UserName, checkAuth };
+// export { displayLogedUser, UserName, checkAuth };
