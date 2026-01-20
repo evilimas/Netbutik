cbb554c181ceeadef5179230d3e04729b5321b2e
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-01-20 14:56:04 +0100

    new cart routes

--- backend/src/routes/cart.routes.ts
+++ backend/src/routes/cart.routes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
-import { addToCart } from '../controllers/cart.controllers';
+import {
+  addToCart,
+  getCartCount,
+  getAll,
+} from '../controllers/cart.controllers';
 import type { Router } from 'express';
 
 export const cartRouter: Router = express.Router();
 cartRouter.post('/add', addToCart);
+cartRouter.get('/cart-count', getCartCount);
+cartRouter.get('/', getAll);
